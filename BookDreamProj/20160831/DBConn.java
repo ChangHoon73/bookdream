701ffe176ea583029cad3f16485a53c412977a72
Chang Hoon <gelheart@naver.com>
2016-08-31 17:22:31 +0900

    J20160831

---
+++ BookDreamProj/20160831/DBConn.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConn {
+	public DBConn(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConnection(){
+		String url = "jdbc:mysql://114.108.176.43/bookdream";
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(url, "bookdream", "bookdream");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+}

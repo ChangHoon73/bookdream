0a9228ab651bc3a4124015f4fcab6745dbda0603
Chang Hoon <gelheart@naver.com>
2016-09-09 14:52:30 +0900

    공통함수추가 BooksDao Add함수 추가

--- BookDreamProj/src/kr/or/bookdream/dao/BooksDAO.java
+++ BookDreamProj/src/kr/or/bookdream/dao/BooksDAO.java
@@ -1,12 +1,14 @@
 package kr.or.bookdream.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Vector;
 
 import kr.or.bookdream.db.ConnectionPool;
+import kr.or.bookdream.util.CommonUtil;
 import kr.or.bookdream.vo.Books;
 import kr.or.bookdream.vo.Category1;
 
@@ -100,5 +102,54 @@ public class BooksDAO {
 		}
 		
 		return vbooks;
+	}
+	
+	public boolean setBooksAdd(Vector<Books> vbooks, int membersno){
+		membersno = 1;
+		boolean bresult = false;
+		ConnectionPool pool = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		PreparedStatement pstmt2 = null;
+		ResultSet rs = null;
+		CommonUtil cu = new CommonUtil();
+		try{
+			pool = ConnectionPool.getInstance();
+			conn = pool.getConnection();
+			pstmt = conn.prepareStatement("insert into books(title,isbn,author,publisher,pdate,edition,status, link,registdate,rpoint,members_no, category1_no, category2_no) values(?,?,?,?,?,?,?,?,?,?,?,?,?)");
+			
+			pstmt.setString(1, vbooks.get(0).getTitle() );	
+			pstmt.setString(2, vbooks.get(0).getIsbn() );	
+			pstmt.setString(3, vbooks.get(0).getAuthor() );	
+			pstmt.setString(4, vbooks.get(0).getPublisher() );	
+			pstmt.setString(5, vbooks.get(0).getPdate() );	
+			pstmt.setInt(6, vbooks.get(0).getEdition() );	
+			pstmt.setString(7, vbooks.get(0).getStatus() );	
+			pstmt.setString(8, vbooks.get(0).getLink() );	
+			pstmt.setString(9, cu.getNowTime() );	
+			pstmt.setInt(10, vbooks.get(0).getRpoint() );	
+			pstmt.setInt(11, membersno );	
+			pstmt.setInt(12, vbooks.get(0).getCategory1_no() );	
+			pstmt.setInt(13, vbooks.get(0).getCategory2_no() );	
+			bresult = pstmt.execute();	
+			
+			if(bresult){
+				pstmt2.getConnection().prepareStatement("insert into history(wdate,books_no, members_no) values(?,?,?)");
+				pstmt2.setString(1, cu.getNowTime());
+				pstmt2.setInt(2, vbooks.get(0).getNo());
+				pstmt2.setInt(1, membersno);
+				pstmt2.execute();
+			}
+		}catch(Exception err){
+			System.err.println(err.toString());
+		}finally {
+			if(pstmt2 != null)
+				try {pstmt2.close();} catch (SQLException e) {e.printStackTrace();}
+			if(pstmt != null)
+				try {pstmt.close();} catch (SQLException e) {e.printStackTrace();}
+			if(pool != null) pool.releaseConnection(conn);
+		}
+		
+		return bresult;
 	}
 }
